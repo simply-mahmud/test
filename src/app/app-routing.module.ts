f7a52a55b29f2595b2af54d04bc6c984b58edcc3
DESKTOP-1JJ1H27\Mahmud CSE <simply.mahmud@gmail.com>
2022-12-27 23:34:06 +0600

    routing with wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './user/user.component'
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: "user",
+    component: UserComponent
+  },
+  {
+    path: "admin",
+    component: AdminComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
